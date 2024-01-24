61080960cb0c613c8defdcd37a331070768907ad
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-01-24 14:18:02 +0100

    Initial commit

--- maingame.py
+++ maingame.py
@@ -1,7 +1,6 @@
 # maingame.py
 import pygame
 import json
-import sys
 from enemy import Enemy
 from turrets import Turret
 from world import World
@@ -21,10 +20,11 @@ speed = c.TILE_SIZE
 x, y = 0, 0
 
 # Fenstereinstellungen treffen
-screen = pygame.display.set_mode((c.SCREEN_WIDTH + c.SIDE_PANEL, c.SCREEN_HEIGHT))
+screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT + c.SIDE_PANEL))
 pygame.display.set_caption("StarGuard")
 
 # Gamevariables
+attract_mode = True
 game_over = False
 game_outcome = 0
 last_enemy_spawn = pygame.time.get_ticks()
@@ -68,13 +68,28 @@ game_over_image = pygame.transform.scale(
     game_over_image, (c.SCREEN_HEIGHT, c.SCREEN_WIDTH)
 )
 
+# attract Mode
+attract_mode_bg = pygame.image.load("assets/images/StarGuardBackGroundImage.jpeg")
+attract_mode_bg = pygame.transform.scale(
+    attract_mode_bg, (c.SCREEN_HEIGHT, c.SCREEN_WIDTH)
+)
+# Logo
+attract_mode_logo = pygame.image.load("assets/images/LogoGepixelt.png")
+
+# Heart,Coin and Textbox
+heart_icon = pygame.image.load("assets/images/Heart.png")
+coin_icon = pygame.image.load("assets/images/Coin.png")
+
+heart_icon = pygame.transform.scale(heart_icon, (30, 30))
+coin_icon = pygame.transform.scale(coin_icon, (30, 30))
+
 # load json data for level
 current_level = 1
 with open(f"levels/map_{current_level}.tmj") as file:
     world_data = json.load(file)
 
 # load in fonts
-text_font = pygame.font.SysFont("upheavalttbrk", 35)
+text_font = pygame.font.SysFont("upheavalttbrk", 25)
 large_font = pygame.font.SysFont("upheavalttbrk", 40)
 
 
@@ -131,71 +146,83 @@ last_movement_time = pygame.time.get_ticks()
 run = True
 while run:
     clock.tick(c.FPS)
-    screen.fill("grey100")
 
-    world.draw(screen)
+    if attract_mode == True:
+        screen.blit(attract_mode_bg, (0, 0))
+        screen.blit(attract_mode_logo, (130, 100))
+        draw_text("Press G to start", text_font, "grey100", 125, 350)
 
-    pygame.draw.lines(screen, "grey0", False, world.waypoints)
-
-    if game_over == False:
-        if world.health <= 0:
-            game_over = True
-            game_outcome = -1  # Verloren
-
-        # draw groups
-        enemy_group.update(world)
-        turret_group.update(enemy_group)
-
-        # Highlight selected turret
-        if selected_turret:
-            if selected_turret.selected == False or selected_turret.selected == None:
-                selected_turret.selected = True
-            else:
-                selected_turret.selected = False
-
-    enemy_group.draw(screen)
-    for turret in turret_group:
-        turret.draw(screen)
-
-    draw_text(str(world.health), text_font, "grey100", 0, 0)
-    draw_text(str(world.money), text_font, "grey100", 0, 30)
-    draw_text(str(world.level), text_font, "grey100", 0, 60)
-
-    if game_over == False:
-        if pygame.time.get_ticks() - last_enemy_spawn > c.SPAWN_COOLDOWN:
-            if world.spawned_enemies < len(world.enemy_list):
-                enemy_type = world.enemy_list[world.spawned_enemies]
-                enemy = Enemy(enemy_type, world.waypoints, enemy_images)
-                # Gegner der Gruppe hinzufügen
-                enemy_group.add(enemy)
-                # Counter erhöhen
-                world.spawned_enemies += 1
-                last_enemy_spawn = pygame.time.get_ticks()
-
-        world.check_level_complete()
-        if world.level_complete == True:
-            world.money += c.LEVEL_COMPLETE_REWARD
-            world.level += 1
-            selected_turret = None
-            last_enemy_spawn = pygame.time.get_ticks()
-            current_level += 1
-            placing_turrets = False
-            world.reset_level()
-            turret_group.empty()
-            world.image = pygame.image.load(
-                f"levels/map_{current_level}.png"
-            ).convert_alpha()
-            with open(f"levels/map_{current_level}.tmj") as file:
-                world.level_data = json.load(file)
-            world.process_data()
-            world.process_enemies()
-            world.level_complete = False
-    # Ergenenzung, wennn der Spieler verliert:
     else:
-        screen.blit(game_over_image, (0, 0))
-        if game_outcome == -1:
-            draw_text("Game-Over", large_font, "white", 120, 190)
-            draw_text("Press R to restart", text_font, "white", 65, 220)
+        screen.fill("grey100")
+        world.draw(screen)
+
+        pygame.draw.lines(screen, "grey0", False, world.waypoints)
+
+        if game_over == False:
+            if world.health <= 0:
+                game_over = True
+                game_outcome = -1  # Verloren
+
+            # draw groups
+            enemy_group.update(world)
+            turret_group.update(enemy_group)
+
+            # Highlight selected turret
+            if selected_turret:
+                if (
+                    selected_turret.selected == False
+                    or selected_turret.selected == None
+                ):
+                    selected_turret.selected = True
+                else:
+                    selected_turret.selected = False
+
+        enemy_group.draw(screen)
+        for turret in turret_group:
+            turret.draw(screen)
+
+        screen.blit(heart_icon, (40, 5))
+        screen.blit(coin_icon, (95, 5))
+        draw_text("Level: ", text_font, "grey100", 200, 7)
+        draw_text(str(world.health), text_font, "grey100", 75, 7)
+        draw_text(str(world.money), text_font, "grey100", 125, 7)
+        draw_text(str(world.level), text_font, "grey100", 285, 7)
+
+        if game_over == False:
+            if pygame.time.get_ticks() - last_enemy_spawn > c.SPAWN_COOLDOWN:
+                if world.spawned_enemies < len(world.enemy_list):
+                    enemy_type = world.enemy_list[world.spawned_enemies]
+                    enemy = Enemy(enemy_type, world.waypoints, enemy_images)
+                    # Gegner der Gruppe hinzufügen
+                    enemy_group.add(enemy)
+                    # Counter erhöhen
+                    world.spawned_enemies += 1
+                    last_enemy_spawn = pygame.time.get_ticks()
+
+            world.check_level_complete()
+            if world.level_complete == True:
+                world.money += c.LEVEL_COMPLETE_REWARD
+                world.level += 1
+                selected_turret = None
+                last_enemy_spawn = pygame.time.get_ticks()
+                current_level += 1
+                placing_turrets = False
+                world.reset_level()
+                turret_group.empty()
+                world.image = pygame.image.load(
+                    f"levels/map_{current_level}.png"
+                ).convert_alpha()
+                with open(f"levels/map_{current_level}.tmj") as file:
+                    world.level_data = json.load(file)
+                world.process_data()
+                world.process_enemies()
+                world.level_complete = False
+        # Ergenenzung, wennn der Spieler verliert:
+        else:
+            screen.blit(game_over_image, (0, 0))
+            if game_outcome == -1:
+                draw_text("Game-Over", large_font, "white", 120, 190)
+                draw_text("Press R to restart", text_font, "white", 65, 220)
 
     for event in pygame.event.get():
         # Möglichkeit das Spiel zu beenden
@@ -228,6 +255,8 @@ while run:
         # check if there is enough money
         if world.money >= c.BUY_COST:
             create_turret()
+    if keys[pygame.K_g] and current_time - last_movement_time > movement_timeout:
+        attract_mode = False
 
     if keys[pygame.K_l]:
         if keys[pygame.K_v] and current_time - last_movement_time > movement_timeout:
@@ -256,8 +285,8 @@ while run:
         enemy_group.empty()
         turret_group.empty()
 
-    screen.blit(cursor_image, (x, y))
-
+    if attract_mode == False:
+        screen.blit(cursor_image, (x, y))
     pygame.display.flip()
 
 pygame.quit()
